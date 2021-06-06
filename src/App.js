25048a529961e7ff328cef4bc2241b0f854631ee
Bimal Boby <bimalboby007@gmail.com>
2021-06-06 12:07:01 +0530

    more movie link added / able to fetch everything dynamically

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import NavBar from './Components/NavBar/NavBar'
 import './App.css'
 import Banner from './Components/Banner/Banner'
 import RowPost from './Components/RowPost/RowPost'
-import {originals,action} from './urls'
+import {originals,action,comedy,horror,documentaries,romance} from './urls'
 
 
 function App() {
@@ -13,6 +13,10 @@ function App() {
      <Banner/>
      <RowPost url={originals} title="Netflix Originals"/>
      <RowPost url={action} title="Action" isSmall/>
+     <RowPost url={comedy} title="Comedy" isSmall/>
+     <RowPost url={horror} title="Horror" isSmall/>
+     <RowPost url={documentaries} title="Documentaries" isSmall/>
+     <RowPost url={romance} title="Romance" isSmall/>
     </div>
   );
 }
